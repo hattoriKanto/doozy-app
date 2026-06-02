112d59fe7a91e42c13cfcbfe16b3c36a4093d097
hattoriKanto <hattorikanto@gmail.com>
2026-06-02 22:53:28 +0300

    feature: setup basic validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AppConfigService } from "./config/config.service";
@@ -5,6 +6,8 @@ import { AppConfigService } from "./config/config.service";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const appConfig = app.get<AppConfigService>(AppConfigService);
+
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
 	await app.listen(appConfig.port);
 }
