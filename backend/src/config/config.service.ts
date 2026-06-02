c13f4a4237a22be0fd2d9112a34a83f062b70da3
hattoriKanto <hattorikanto@gmail.com>
2026-06-02 21:06:22 +0300

    feature: add basic api setup

---
+++ backend/src/config/config.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import z from "zod";
+
+const validationObject = z.object({
+	PORT: z.string().min(0).max(65535),
+});
+
+@Injectable()
+export class AppConfigService {
+	static validate = (
+		config: Record<string, unknown>,
+	): z.infer<typeof validationObject> => validationObject.parse(config);
+
+	constructor(
+		private readonly configService: ConfigService<
+			z.infer<typeof validationObject>,
+			true
+		>,
+	) {}
+
+	get port(): number {
+		return Number(this.configService.get("PORT"));
+	}
+}
---
+++ backend/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AppConfigService } from "./config/config.service";
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	const appConfig = app.get<AppConfigService>(AppConfigService);
+
+	await app.listen(appConfig.port);
+}
+bootstrap();
