9700db483f676f5a9c5c071259434d8139d32237
hattoriKanto <hattorikanto@gmail.com>
2026-06-03 08:49:44 +0300

    fix: add relation between Category and Todo tables

---
+++ backend/prisma/migrations/20260603054920_add_category_table/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `Todo` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'notCompleted',
+    "categoryId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Todo_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Todo" ("createdAt", "description", "id", "status", "title", "updatedAt") SELECT "createdAt", "description", "id", "status", "title", "updatedAt" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
